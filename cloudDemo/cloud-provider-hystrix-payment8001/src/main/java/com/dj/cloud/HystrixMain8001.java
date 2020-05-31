5047acba12393944f2e04cb00507739b8e45e54e
””wj <15735228522@163.com>
2020-05-31 23:44:13 +0800

    HystrixDashboard

--- cloudDemo/cloud-provider-hystrix-payment8001/src/main/java/com/dj/cloud/HystrixMain8001.java
+++ cloudDemo/cloud-provider-hystrix-payment8001/src/main/java/com/dj/cloud/HystrixMain8001.java
@@ -1,12 +1,16 @@
 package com.dj.cloud;
 
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 @MapperScan("com.dj.cloud.mapper")
@@ -20,4 +24,23 @@ public class HystrixMain8001 {
         System.out.println("HystrixMain8001  start  successfully.............");
 
     }
+
+    /**
+     * 此配置是为了服务监控而配置 与服务本身容错无关
+     * ServletRegistrationBean 因为springboot 默认路径不是/hystrix.stream
+     * 只要在自己项目里配置上下面的Servlet就行
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean  servletRegistrationBean = new ServletRegistrationBean(streamServlet);
+        servletRegistrationBean.setLoadOnStartup(1);
+        servletRegistrationBean.addUrlMappings("/hystrix.stream");
+        servletRegistrationBean.setName("HystrixMetricsStreamServlet");
+        return servletRegistrationBean;
+    }
+
+
 }
